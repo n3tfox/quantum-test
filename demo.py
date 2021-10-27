59f0601c15f7965ed5918e01429c6909866d7449
naru fox <93215762+n3tfox@users.noreply.github.com>
2021-10-27 11:48:04 -0400

    Update demo.py

--- demo.py
+++ demo.py
@@ -11,18 +11,32 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from dimod.reference.samplers import ExactSolver
+from dwave.system import DWaveSampler, EmbeddingComposite
+
 import dwavebinarycsp
+print("defining circuit\n")
+def logic_circuit(a,b,z):
+    and1=a and b
 
-def logic_circuit(a, b, c, d, z):
-    not1 = not b
-    or2 = b or c
-    and3 = a and not1
-    or4 = or2 or d
-    and5 = and3 and or4
-    not6 = not or4
-    or7 = and5 or not6
-    return (z == or7)
-
+    or1= a or b
+    not1= not and1
+    xor1=or1 and not1
+    output1=xor1
+    return (output1==z)
+print("csp setting var\n")
 csp = dwavebinarycsp.ConstraintSatisfactionProblem(dwavebinarycsp.BINARY)
-csp.add_constraint(logic_circuit, ['a', 'b', 'c', 'd', 'z'])
+print("csp adding constraint\n")
+csp.add_constraint(logic_circuit, ['a', 'b', 'z'])
+print("converting to bqm\n")
+# Convert the binary constraint satisfaction problem to a binary quadratic model
+bqm = dwavebinarycsp.stitch(csp)
+print("printing bqm\n")
+print(bqm)
+print("making sampler\n")
+sampler = EmbeddingComposite(DWaveSampler())
+print("making sampleset\n")
+sampleset = sampler.sample(bqm, num_reads=10, label='output')
+print("printing sampleset\n")
 
+print(sampleset)
